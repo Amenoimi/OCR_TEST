8041c91e39bf28209f8df09f4abe8ea6b3d44b6f
amenoimi <bmpss92285@gmail.com>
2018-07-20 16:09:20 +0800

    只是一個更新

--- app/src/main/java/com/example/amenoimi/ocr_test/MainActivity.java
+++ app/src/main/java/com/example/amenoimi/ocr_test/MainActivity.java
@@ -1,15 +1,40 @@
 package com.example.amenoimi.ocr_test;
 
+import android.graphics.Bitmap;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+
+import com.googlecode.tesseract.android.TessBaseAPI;
 
 public class MainActivity extends AppCompatActivity {
     static final String TESSBASE_PATH = "/storage/emulated/0/download/tesseract/";
     static final String DEFAULT_LANGUAGE = "eng";
     static final String CHINESE_LANGUAGE = "chi_tra";
+    private View imgSrc;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public String ocrWithEnglish() {
+        String resString = "";
+
+        imgSrc.setDrawingCacheEnabled(true);
+        final Bitmap bitmap = imgSrc.getDrawingCache();
+        final TessBaseAPI ocrApi = new TessBaseAPI();
+
+        ocrApi.init(TESSBASE_PATH, CHINESE_LANGUAGE);
+        ocrApi.setPageSegMode(TessBaseAPI.PageSegMode.PSM_SINGLE_LINE);
+
+        ocrApi.setImage(bitmap);
+        resString = ocrApi.getUTF8Text();
+
+        ocrApi.clear();
+        ocrApi.end();
+        return  resString;
+    }
+
 }
