0a21fb5ce434af595320d7d734d4d275cb68133a
amenoimi <bmpss92285@gmail.com>
2018-07-20 15:16:45 +0800

    new day(?

---
+++ app/src/main/java/com/example/amenoimi/ocr_test/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.amenoimi.ocr_test;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
