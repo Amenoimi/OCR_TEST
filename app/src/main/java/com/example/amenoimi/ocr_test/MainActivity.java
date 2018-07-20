3ed5a60fe627718941d5b51cf9b786b14d6c1b38
amenoimi <bmpss92285@gmail.com>
2018-07-20 15:43:51 +0800

    add -A

--- app/src/main/java/com/example/amenoimi/ocr_test/MainActivity.java
+++ app/src/main/java/com/example/amenoimi/ocr_test/MainActivity.java
@@ -4,7 +4,9 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-
+    static final String TESSBASE_PATH = "/storage/emulated/0/download/tesseract/";
+    static final String DEFAULT_LANGUAGE = "eng";
+    static final String CHINESE_LANGUAGE = "chi_tra";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
