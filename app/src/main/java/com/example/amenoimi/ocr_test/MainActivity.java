89f7bf43d008c49da3a68ddfe43a32073708b6aa
amenoimi <bmpss92285@gmail.com>
2018-07-25 13:50:34 +0800

    一閃一閃小眼睛

--- app/src/main/java/com/example/amenoimi/ocr_test/MainActivity.java
+++ app/src/main/java/com/example/amenoimi/ocr_test/MainActivity.java
@@ -1,21 +1,42 @@
 package com.example.amenoimi.ocr_test;
 
+import android.Manifest;
 import android.app.Dialog;
 import android.app.ProgressDialog;
 import android.content.ContentResolver;
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.ImageFormat;
 import android.hardware.Camera;
+import android.hardware.camera2.CameraAccessException;
+import android.hardware.camera2.CameraCaptureSession;
+import android.hardware.camera2.CameraCharacteristics;
+import android.hardware.camera2.CameraDevice;
+import android.hardware.camera2.CameraManager;
+import android.hardware.camera2.CaptureRequest;
+import android.media.Image;
+import android.media.ImageReader;
 import android.media.ThumbnailUtils;
 import android.net.Uri;
 import android.os.AsyncTask;
+import android.os.Build;
 import android.os.Environment;
+import android.os.Handler;
+import android.os.HandlerThread;
 import android.provider.MediaStore;
+import android.support.annotation.RequiresApi;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.util.SparseIntArray;
+import android.view.Surface;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -23,6 +44,7 @@ import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.googlecode.tesseract.android.TessBaseAPI;
 
@@ -40,6 +62,8 @@ import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.net.URL;
 import java.net.URLConnection;
+import java.nio.ByteBuffer;
+import java.util.Arrays;
 import java.util.Calendar;
 
 import static android.os.Environment.getDataDirectory;
@@ -53,11 +77,34 @@ public class MainActivity extends AppCompatActivity  implements View.OnClickList
     static final String CHINESE_LANGUAGE_SIM = "chi_sim";
     private ImageView imgSrc;
     public TextView t1;
-    public Button b1,b2,b3;
+    public Button b1,b2,b3,b4;
     public static final int progressType = 0;
     public static final int CAMERA_PIC_REQUEST = 12;
     private ProgressDialog progressDialog;
     public int ImgToTextMode=0;
+
+
+    private static final SparseIntArray ORIENTATIONS = new SparseIntArray();
+
+    ///为了使照片竖直显示
+    static {
+        ORIENTATIONS.append(Surface.ROTATION_0, 90);
+        ORIENTATIONS.append(Surface.ROTATION_90, 0);
+        ORIENTATIONS.append(Surface.ROTATION_180, 270);
+        ORIENTATIONS.append(Surface.ROTATION_270, 180);
+    }
+
+    private SurfaceView mSurfaceView;
+    private SurfaceHolder mSurfaceHolder;
+    private ImageView iv_show;
+    private CameraManager mCameraManager;//摄像头管理器
+    private Handler childHandler, mainHandler;
+    private String mCameraID;//摄像头Id 0 为后  1 为前
+    private ImageReader mImageReader;
+    private CameraCaptureSession mCameraCaptureSession;
+    private CameraDevice mCameraDevice;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -67,9 +114,11 @@ public class MainActivity extends AppCompatActivity  implements View.OnClickList
         b1=(Button)findViewById(R.id.b1);
         b2=(Button)findViewById(R.id.b2);
         b3=(Button)findViewById(R.id.b3);
+        b4=(Button)findViewById(R.id.b4);
         b1.setOnClickListener(this);
         b2.setOnClickListener(this);
         b3.setOnClickListener(this);
+        b4.setOnClickListener(this);
         Spinner spinner = (Spinner)findViewById(R.id.sp);
         ArrayAdapter<CharSequence> lunchList = ArrayAdapter.createFromResource(MainActivity.this,
                 R.array.lunch,
@@ -97,17 +146,154 @@ public class MainActivity extends AppCompatActivity  implements View.OnClickList
             e.printStackTrace();
         }
 
-
-//        Camera object = null;
-//        try {
-//            object = Camera.open();
-//        }
-//        catch (Exception e){
-//        }
-
-
-
     }
+
+
+
+    /**
+     * 初始化
+     */
+    private void initVIew() {
+        iv_show = (ImageView) findViewById(R.id.imageView);
+        //mSurfaceView
+        mSurfaceView = (SurfaceView) findViewById(R.id.surfaceView);
+        mSurfaceView.setOnClickListener(this);
+        mSurfaceHolder = mSurfaceView.getHolder();
+        mSurfaceHolder.setKeepScreenOn(true);
+        // mSurfaceView添加回调
+        mSurfaceHolder.addCallback(new SurfaceHolder.Callback() {
+            @Override
+            public void surfaceCreated(SurfaceHolder holder) { //SurfaceView创建
+                // 初始化Camera
+                initCamera2();
+            }
+
+            @Override
+            public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
+            }
+
+            @Override
+            public void surfaceDestroyed(SurfaceHolder holder) { //SurfaceView销毁
+                // 释放Camera资源
+                if (null != mCameraDevice) {
+                    mCameraDevice.close();
+                    MainActivity.this.mCameraDevice = null;
+                }
+            }
+        });
+    }
+
+
+    /**
+     * 初始化Camera2
+     */
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+    private void initCamera2() {
+        HandlerThread handlerThread = new HandlerThread("Camera2");
+        handlerThread.start();
+        childHandler = new Handler(handlerThread.getLooper());
+        mainHandler = new Handler(getMainLooper());
+        mCameraID = "" + CameraCharacteristics.LENS_FACING_FRONT;//后摄像头
+        mImageReader = ImageReader.newInstance(1080, 1920, ImageFormat.JPEG,1);
+        mImageReader.setOnImageAvailableListener(new ImageReader.OnImageAvailableListener() { //可以在这里处理拍照得到的临时照片 例如，写入本地
+            @Override
+            public void onImageAvailable(ImageReader reader) {
+                mCameraDevice.close();
+                mSurfaceView.setVisibility(View.GONE);
+                iv_show.setVisibility(View.VISIBLE);
+                // 拿到拍照照片数据
+                Image image = reader.acquireNextImage();
+                ByteBuffer buffer = image.getPlanes()[0].getBuffer();
+                byte[] bytes = new byte[buffer.remaining()];
+                buffer.get(bytes);//由缓冲区存入字节数组
+                final Bitmap bitmap = BitmapFactory.decodeByteArray(bytes, 0, bytes.length);
+                if (bitmap != null) {
+                    iv_show.setImageBitmap(bitmap);
+                }
+            }
+        }, mainHandler);
+        //获取摄像头管理
+        mCameraManager = (CameraManager) getSystemService(Context.CAMERA_SERVICE);
+        try {
+            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+                return;
+            }
+            //打开摄像头
+            mCameraManager.openCamera(mCameraID, stateCallback, mainHandler);
+        } catch (CameraAccessException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * 摄像头创建监听
+     */
+    private CameraDevice.StateCallback stateCallback = new CameraDevice.StateCallback() {
+        @Override
+        public void onOpened(CameraDevice camera) {//打开摄像头
+            mCameraDevice = camera;
+            //开启预览
+            takePreview();
+        }
+
+        @Override
+        public void onDisconnected(CameraDevice camera) {//关闭摄像头
+            if (null != mCameraDevice) {
+                mCameraDevice.close();
+                MainActivity.this.mCameraDevice = null;
+            }
+        }
+
+        @Override
+        public void onError(CameraDevice camera, int error) {//发生错误
+            Toast.makeText(MainActivity.this, "摄像头开启失败", Toast.LENGTH_SHORT).show();
+        }
+    };
+
+
+
+    /**
+     * 开始预览
+     */
+    private void takePreview() {
+        try {
+            // 创建预览需要的CaptureRequest.Builder
+            final CaptureRequest.Builder previewRequestBuilder = mCameraDevice.createCaptureRequest(CameraDevice.TEMPLATE_PREVIEW);
+            // 将SurfaceView的surface作为CaptureRequest.Builder的目标
+            previewRequestBuilder.addTarget(mSurfaceHolder.getSurface());
+            // 创建CameraCaptureSession，该对象负责管理处理预览请求和拍照请求
+            mCameraDevice.createCaptureSession(Arrays.asList(mSurfaceHolder.getSurface(), mImageReader.getSurface()), new CameraCaptureSession.StateCallback() // ③
+            {
+                @Override
+                public void onConfigured(CameraCaptureSession cameraCaptureSession) {
+                    if (null == mCameraDevice) return;
+                    // 当摄像头已经准备好时，开始显示预览
+                    mCameraCaptureSession = cameraCaptureSession;
+                    try {
+                        // 自动对焦
+                        previewRequestBuilder.set(CaptureRequest.CONTROL_AF_MODE, CaptureRequest.CONTROL_AF_MODE_CONTINUOUS_PICTURE);
+                        // 打开闪光灯
+                        previewRequestBuilder.set(CaptureRequest.CONTROL_AE_MODE, CaptureRequest.CONTROL_AE_MODE_ON_AUTO_FLASH);
+                        // 显示预览
+                        CaptureRequest previewRequest = previewRequestBuilder.build();
+                        mCameraCaptureSession.setRepeatingRequest(previewRequest, null, childHandler);
+                    } catch (CameraAccessException e) {
+                        e.printStackTrace();
+                    }
+                }
+
+                @Override
+                public void onConfigureFailed(CameraCaptureSession cameraCaptureSession) {
+                    Toast.makeText(MainActivity.this, "配置失败", Toast.LENGTH_SHORT).show();
+                }
+            }, childHandler);
+        } catch (CameraAccessException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+
 
     public void up_mode(View v){
         try {
@@ -191,6 +377,15 @@ public class MainActivity extends AppCompatActivity  implements View.OnClickList
         if( v.getId()==R.id.b1)get_img(v);
         if( v.getId()==R.id.b2)get_img_now(v);
         if( v.getId()==R.id.b3)up_mode(v);
+        if(v.getId()==R.id.b4){
+//            if(b4.getBackground()==ContextCompat.getDrawable(this,R.drawable.unsee)){
+//                b4.setBackgroundDrawable(ContextCompat.getDrawable(this,R.drawable.see));
+                initVIew();
+//            }else{
+//                b4.setBackgroundDrawable(ContextCompat.getDrawable(this,R.drawable.unsee));
+//            }
+
+        }
     }
     //執行OCR
     public String ocrWithEnglish() {
@@ -367,6 +562,7 @@ public class MainActivity extends AppCompatActivity  implements View.OnClickList
                 progressDialog.setMax(100);
                 progressDialog.setProgressStyle(ProgressDialog.STYLE_HORIZONTAL);
                 progressDialog.setCancelable(true);
+                progressDialog.setCanceledOnTouchOutside(false);
                 progressDialog.show();
                 return progressDialog;
             default:
@@ -421,4 +617,8 @@ public class MainActivity extends AppCompatActivity  implements View.OnClickList
         newBmp.setPixels(pixels, 0, width, 0, 0, width, height);
         Bitmap resizeBmp = ThumbnailUtils.extractThumbnail(newBmp, w, h);
         return resizeBmp;
-    }}
+    }
+
+
+
+}
