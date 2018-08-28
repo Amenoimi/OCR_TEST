5ee94aacad5513dae4b915404a5c86e4551ece42
EienYuki <D1044182012@gm.lhu.edu.tw>
2018-08-28 14:36:40 +0800

    修改 ScanMarkingPoint（仮）

--- app/src/main/java/com/example/amenoimi/ocr_test/ScanMarkingPoint.java
+++ app/src/main/java/com/example/amenoimi/ocr_test/ScanMarkingPoint.java
@@ -17,8 +17,8 @@ public class ScanMarkingPoint {
     private Paint p;
 
     ScanMarkingPoint() {
-        // 距離上 距離下 範圍長 範圍寬 小正方形長 小正方形寬
-        this.config = new float[] {0.25f, 0.25f, 0.5f, 0.5f, 0.2f, 0.2f};
+        // 距離上 距離下 範圍長 範圍寬 小正方形長寬(依照寬度比例)
+        this.config = new float[] {0.25f, 0.25f, 0.5f, 0.5f, 0.1f};
 
         this.p = new Paint();
     }
@@ -26,8 +26,8 @@ public class ScanMarkingPoint {
         this.ScreenTarget = ScreenTarget;
         this.ScanTarget = ScanTarget;
 
-        // 距離上 距離下 範圍長 範圍寬 小正方形長 小正方形寬
-        this.config = new float[] {0.25f, 0.25f, 0.5f, 0.5f, 0.2f, 0.2f};
+        // 距離上 距離下 範圍長 範圍寬 小正方形長寬(依照寬度比例)
+        this.config = new float[] {0.25f, 0.25f, 0.5f, 0.5f, 0.1f};
 
         this.p = new Paint();
     }
@@ -36,8 +36,8 @@ public class ScanMarkingPoint {
         int w = ScreenTarget.getWidth();
         int h = ScreenTarget.getHeight();
 
-        int mw = (int) (w * this.config[4]) ;
-        int mh = (int) (h * this.config[5]) ;
+        int mw = (int)(w * this.config[4]);
+        int mh = mw;
 
         float left = w * this.config[0];
         float top = h * this.config[0];
@@ -46,13 +46,80 @@ public class ScanMarkingPoint {
 
 
         p.setColor(Color.parseColor("#006ac6"));
-        p.setStyle(Paint.Style.FILL);
+        p.setStyle(Paint.Style.STROKE);
         ScreenTarget.drawRect(left, top, right, bottom, p);
 
         p.setColor(Color.parseColor("#183292"));
+        p.setStyle(Paint.Style.FILL);//設置填滿
         ScreenTarget.drawRect(left, top, left+mw, top+mh, p);
         ScreenTarget.drawRect(left, bottom - mh, left+mw, bottom, p);
         ScreenTarget.drawRect(right - mw, top, right, top+mh, p);
         ScreenTarget.drawRect(right - mw, bottom - mh, right, bottom, p);
+    }
+
+    public boolean findrect(Bitmap ScanTarget) {
+        int w = ScanTarget.getWidth();
+        int h = ScanTarget.getHeight();
+
+        int mw = (int)(w * this.config[4]);
+        int mh = mw;
+
+        int left = (int)(w * this.config[0]);
+        int top = h * this.config[0]);
+        int right = (int)(left + w * this.config[2]);
+        int bottom = (int)(top + h * this.config[3]);
+
+        int[] pixels = new int[w * h];
+        ScanTarget.getPixels(pixels, 0, w, 0, 0, w, h);
+
+        float S = 0, V = 0;
+        int n = 0;
+        float[] hsv = new float[3];
+
+        for (int y=top; y<top+mh; y++ ){
+            for (int x=left; x<left+mw; x++ ){
+                Color.colorToHSV(ScanTarget.getPixel(x, y), hsv);
+                S += hsv[1];
+                V += hsv[2];
+                n++;
+            }
+        }
+
+        for (int y=bottom - mh; y<bottom; y++ ){
+            for (int x=left; x<left+mw; x++ ){
+                Color.colorToHSV(ScanTarget.getPixel(x, y), hsv);
+                S += hsv[1];
+                V += hsv[2];
+                n++;
+            }
+        }
+
+        for (int y=bottom - mh; y<top+mh; y++ ){
+            for (int x=right - mw; x<right; x++ ){
+                Color.colorToHSV(ScanTarget.getPixel(x, y), hsv);
+                S += hsv[1];
+                V += hsv[2];
+                n++;
+            }
+        }
+
+        for (int y=top; y<bottom; y++ ){
+            for (int x=right - mw; x<right; x++ ){
+                Color.colorToHSV(ScanTarget.getPixel(x, y), hsv);
+                S += hsv[1];
+                V += hsv[2];
+                n++;
+            }
+        }
+
+        S = S/n;
+        V = V/n;
+
+        if (S > 0.3 && V > 0.7) {
+            return true;
+        } else {
+            return false;
+        }
+
     }
 }
