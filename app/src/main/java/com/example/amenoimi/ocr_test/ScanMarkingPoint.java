725253c6bb22556f54722a7f37046b1fc584df9f
EienYuki <D1044182012@gm.lhu.edu.tw>
2018-08-29 15:55:14 +0800

    修改 ScanMarkingPoint

--- app/src/main/java/com/example/amenoimi/ocr_test/ScanMarkingPoint.java
+++ app/src/main/java/com/example/amenoimi/ocr_test/ScanMarkingPoint.java
@@ -11,8 +11,6 @@ import android.util.Log;
  */
 
 public class ScanMarkingPoint {
-    private Canvas ScreenTarget;
-    private Bitmap ScanTarget;
     private float[] config;
 
     private Paint p;
@@ -29,16 +27,8 @@ public class ScanMarkingPoint {
 
         this.p = new Paint();
     }
-    ScanMarkingPoint(Canvas ScreenTarget, Bitmap ScanTarget, int[] config) {
-        this.ScreenTarget = ScreenTarget;
-        this.ScanTarget = ScanTarget;
 
-        // 距離左 距離上 範圍寬 範圍高 小正方形長寬(依照寬度比例)
-        this.config = new float[] {0.25f, 0.25f, 0.5f, 0.5f, 0.1f};
-
-        this.p = new Paint();
-    }
-
+    // 繪製邊框
     public void drawing(Canvas ScreenTarget) {
         int w = ScreenTarget.getWidth();
         int h = ScreenTarget.getHeight();
@@ -64,7 +54,9 @@ public class ScanMarkingPoint {
         ScreenTarget.drawRect(right - mw, bottom - mh, right, bottom, p);
     }
 
-    public int[] findrect(Bitmap ScanTarget, Double[] scale_error) {
+    // 識別邊框
+    // for 一般情況
+    public int[] findrect(Bitmap ScanTarget) {
         int ok = 0;
         int w = ScanTarget.getWidth();
         int h = ScanTarget.getHeight();
@@ -77,8 +69,6 @@ public class ScanMarkingPoint {
         int right = (int)(left + w * this.config[2]);
         int bottom = (int)(top + h * this.config[3]);
 
-        //int[] pixels = new int[w * h];
-        //ScanTarget.getPixels(pixels, 0, w, 0, 0, w, h);
 
         float S = 0, V = 0;
         int n = 0;
@@ -127,53 +117,6 @@ public class ScanMarkingPoint {
         if (S > 0.20 && V < 0.70) ok+=1;
         // 找定位點 END
 
-        /*/// 確認在白紙
-        S = 0;
-        V = 0;
-        n = 0;
-
-        for (int y=top+mh; y<bottom - mh; y++ ){
-            for (int x=left; x<left+mw; x++ ){
-                Color.colorToHSV(ScanTarget.getPixel(x, y), hsv);
-                S += hsv[1];
-                V += hsv[2];
-                n++;
-            }
-        }
-
-        for (int y=top; y<top+mh; y++ ){
-            for (int x=left+mw; x<right - mw; x++ ){
-                Color.colorToHSV(ScanTarget.getPixel(x, y), hsv);
-                S += hsv[1];
-                V += hsv[2];
-                n++;
-            }
-        }
-
-        for (int y=bottom - mh; y<bottom; y++ ){
-            for (int x=left+mw; x<right - mw; x++ ){
-                Color.colorToHSV(ScanTarget.getPixel(x, y), hsv);
-                S += hsv[1];
-                V += hsv[2];
-                n++;
-            }
-        }
-
-        for (int y=top+mh; y<bottom - mh; y++ ){
-            for (int x=right - mw; x<right; x++ ){
-                Color.colorToHSV(ScanTarget.getPixel(x, y), hsv);
-                S += hsv[1];
-                V += hsv[2];
-                n++;
-            }
-        }
-
-        S = S/n;
-        V = V/n;
-
-        if (S < 0.50 && V > 0.50) ok+=1;
-        /*/// 確認在白紙 END
-
         if (ok == 1) {
             return new int[] {
                     left + mw,
@@ -188,7 +131,8 @@ public class ScanMarkingPoint {
 
     }
 
-    public int[] findrect2(Bitmap ScanTarget, Double[] scale_error) {
+    // for 一般情況
+    public int[] findrect2(Bitmap ScanTarget) {
         int ok = 0;
         int w = ScanTarget.getWidth();
         int h = ScanTarget.getHeight();
@@ -318,7 +262,8 @@ public class ScanMarkingPoint {
 
     }
 
-    public int[] findrect3(Bitmap ScanTarget, Double[] scale_error) {
+    // for 二值化
+    public int[] findrect3(Bitmap ScanTarget) {
         int ok = 0;
         int w = ScanTarget.getWidth();
         int h = ScanTarget.getHeight();
